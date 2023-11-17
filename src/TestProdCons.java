ee3c92069ffb4a02c7a830c1a0868d7534a2d3d0
RDel-Medico <redelmedico@gmail.com>
2023-11-17 09:26:30 +0100

    Creation of all classes

---
+++ src/TestProdCons.java
@@ -0,0 +1,12 @@
+public class TestProdCons {
+/*
+
+Exemple :
+
+Properties properties = new Properties();
+properties.loadFromXML(
+TestProdCons.class.getClassLoader().getResourceAsStream(“options.xml”));
+int nProd = Integer.parseInt(properties.getProperty(“nProd”));
+int nCons = Integer.parseInt(properties.getProperty(“nCons”));
+*/
+}
