0583c8c675d432fbce1f2a60b360ef19a0eac535
arlealexandre <arle.alexandre@protonmail.com>
2023-11-17 10:46:07 +0100

    objectif 1 garde-action

--- src/TestProdCons.java
+++ src/TestProdCons.java
@@ -1,12 +1,17 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.InvalidPropertiesFormatException;
+import java.util.Properties;
+
 public class TestProdCons {
-/*
 
-Exemple :
+    public static void main(String[] args) throws InvalidPropertiesFormatException, IOException {
 
-Properties properties = new Properties();
-properties.loadFromXML(
-TestProdCons.class.getClassLoader().getResourceAsStream(“options.xml”));
-int nProd = Integer.parseInt(properties.getProperty(“nProd”));
-int nCons = Integer.parseInt(properties.getProperty(“nCons”));
-*/
+        Properties properties = new Properties();
+        properties.loadFromXML(new FileInputStream("options.xml"));
+
+        int nProd = Integer.parseInt(properties.getProperty("nProd"));
+        int nCons = Integer.parseInt(properties.getProperty("nCons"));
+
+    }
 }
