a28c1860b29aad235c0297133459238e4c6f41d4
RDel-Medico <redelmedico@gmail.com>
2023-12-04 14:25:11 +0100

    Fixed output display error

--- src/obj3/ProdConsBufferSemaphore.java
+++ src/obj3/ProdConsBufferSemaphore.java
@@ -44,12 +44,13 @@ public class ProdConsBufferSemaphore implements IProdConsBuffer {
     
         try { notFull.acquire(); } catch (InterruptedException e) { e.printStackTrace(); }
 
-        try { mutex.acquire(); } catch (InterruptedException e) { e.printStackTrace(); }
+        
         int prodTime = (int)Math.floor(Math.random() * this.maxProdTime); // We randomised prodTime for better result but we keep an average of "prodTime"
         try { Thread.sleep(prodTime); } catch (InterruptedException e) { e.printStackTrace(); }
 
         System.out.println("Prod"+Thread.currentThread().getName()+" a écrit "+m.toString()+" dans la case n°"+Integer.toString(in));
 
+        try { mutex.acquire(); } catch (InterruptedException e) { e.printStackTrace(); }
         this.buffer[in] = m;
         this.in = (in + 1) % size;
         this.nbMessage++;
@@ -63,7 +64,7 @@ public class ProdConsBufferSemaphore implements IProdConsBuffer {
 
         try { notEmpty.acquire(); } catch (InterruptedException e) { e.printStackTrace(); }
 
-        try { mutex.acquire(); } catch (InterruptedException e) { e.printStackTrace(); }
+        
         int consTime = (int)Math.floor(Math.random() * this.maxConsTime); // We randomised consTime for better result but we keep an average of "consTime"
         try { Thread.sleep(consTime); } catch (InterruptedException e) { e.printStackTrace(); }
 
@@ -71,6 +72,7 @@ public class ProdConsBufferSemaphore implements IProdConsBuffer {
 
         System.out.println("Cons"+Thread.currentThread().getName()+" a lu "+res.toString()+" dans la case n°"+Integer.toString(out));
 
+        try { mutex.acquire(); } catch (InterruptedException e) { e.printStackTrace(); }
         this.out = (out + 1) % size;
         this.nbMessage--;
         mutex.release();
